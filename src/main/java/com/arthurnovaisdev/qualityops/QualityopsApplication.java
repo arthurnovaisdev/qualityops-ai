21dc2f75b5c33570778188c8749a04f7b8da4a49
arthurnovaisdev <arthurnovaispeixoto@gmail.com>
2026-09-14 22:01:37 -0300

    chore: initialize QualityOps backend

---
+++ src/main/java/com/arthurnovaisdev/qualityops/QualityopsApplication.java
@@ -0,0 +1,13 @@
+package com.arthurnovaisdev.qualityops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthurnovaisdev/qualityops/QualityopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurnovaisdev.qualityops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
