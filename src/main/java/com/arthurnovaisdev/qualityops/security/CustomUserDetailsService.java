08315425366636eddf79260be6e2dcd05c1bfad2
arthurnovaisdev <arthurnovaispeixoto@gmail.com>
2026-09-16 15:09:22 -0300

    CustomUserDetailsService

---
+++ src/main/java/com/arthurnovaisdev/qualityops/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.arthurnovaisdev.qualityops.security;
+
+import com.arthurnovaisdev.qualityops.repository.UserRepository;
+import com.arthurnovaisdev.qualityops.entity.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não econtrado"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPasswordHash())
+                .roles(user.getRole().name())
+                .disabled(!user.isActive())
+                .build();
+    }
+}
